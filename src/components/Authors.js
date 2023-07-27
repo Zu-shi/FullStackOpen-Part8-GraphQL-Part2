45ffb52bbe688d619cc178b3ffb96923ff1b79ca
Zu-shi <accountistaken@gmail.com>
2023-07-27 15:43:49 -0400

    8.8 done, able to query authors

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,9 +1,17 @@
+import { useQuery } from "@apollo/client"
+import { ALL_AUTHORS } from "../queries"
+
 const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
+
   if (!props.show) {
     return null
   }
-  const authors = []
 
+  if (authors.loading) {
+    return <div>loading...</div>
+  }
+  console.log("authors", authors)
   return (
     <div>
       <h2>authors</h2>
@@ -14,7 +22,7 @@ const Authors = (props) => {
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
+          {authors.data.allAuthors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
