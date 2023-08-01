cd22170c9fb0a0b92500495be45fed4d8f6cd69f
Zu-shi <accountistaken@gmail.com>
2023-08-01 01:18:16 -0400

    8.21 filter books through GraphQL

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,38 +1,51 @@
 import { useQuery } from "@apollo/client"
-import { ALL_BOOKS } from "../queries"
+import { ALL_BOOKS, ALL_GENRES } from "../queries"
 import { useEffect, useState } from "react"
 
 const Books = (props) => {
-  const books = useQuery(ALL_BOOKS)
-  const [genres, setGenres] = useState([]) // Array of true/false ordered alphabetically, for selection
-
-  let genresTempMap = {};
-  let genresTempArr = [];
+  const [selectedGenres, setSelectedGenres] = useState([])
+  const books = useQuery(ALL_BOOKS,
+    { variables: { genres: selectedGenres.filter((g) => g.selected).map(g => g.name) } })
+  const allGenres = useQuery(ALL_GENRES)
 
   useEffect(() => {
-    if (books.data) {
-      console.log(books.data)
-      books.data.allBooks.forEach((b) => {
-        b.genres.forEach((g) => {
-          genresTempMap[g] = true;
-        })
-      })
+    if (allGenres.isLoading || !allGenres.data) { return }
+    const genreMap = allGenres.data.allGenres.map((g) => ({
+      name: g,
+      selected: g === props.recs
+    }))
+    setSelectedGenres(genreMap)
+  }, [allGenres, props.token, props.recs])
 
-      for (const [key,] of Object.entries(genresTempMap)) {
-        genresTempArr.push(key)
-      }
+  //const [genres, setGenres] = useState([]) // Array of true/false ordered alphabetically, for selection
 
-      // What happens if new genre is added? Need to map it w/ previous genres to figure it out.
-      genresTempArr.sort()
+  // let genresTempMap = {};
+  // let genresTempArr = [];
 
-      console.log("test")
-      console.log(props.recs)
-      console.log(genresTempArr)
-      setGenres(genresTempArr.map((g, index) => {
-        return { index, name: g, selected: g === props.recs ? true : false } // to update logic to keep in place on new
-      }))
-    }
-  }, [books, props.token])
+  // useEffect(() => {
+  //   if (books.data) {
+  //     console.log(books.data)
+  //     books.data.allBooks.forEach((b) => {
+  //       b.genres.forEach((g) => {
+  //         genresTempMap[g] = true;
+  //       })
+  //     })
+
+  //     for (const [key,] of Object.entries(genresTempMap)) {
+  //       genresTempArr.push(key)
+  //     }
+
+  //     // What happens if new genre is added? Need to map it w/ previous genres to figure it out.
+  //     genresTempArr.sort()
+
+  //     console.log("test")
+  //     console.log(props.recs)
+  //     console.log(genresTempArr)
+  //     setGenres(genresTempArr.map((g, index) => {
+  //       return { index, name: g, selected: g === props.recs ? true : false } // to update logic to keep in place on new
+  //     }))
+  //   }
+  // }, [books, props.token])
 
   // The problem is that when books update, genres need to update, but updating genres causes a redraw
   // The useState is being used for two different things here:
@@ -44,15 +57,19 @@ const Books = (props) => {
     return null
   }
 
-  if (books.loading || !genres) {
+  if (allGenres.loading || !allGenres.data) {
+    return <div>genres are loading</div>
+  }
+
+  if (books.loading) {
     return <div>books are loading</div>
   }
 
-
-
   const handleChange = (evt) => {
-    const genreCopy = JSON.parse(JSON.stringify(genres)) // this is a shallow copy
-    const found = genreCopy.find((g) => (g.name === evt.target.id))
+    console.log("selected", selectedGenres)
+    console.log(evt.target.checked)
+    const selectedGenresCopy = JSON.parse(JSON.stringify(selectedGenres)) // this is a shallow copy
+    const found = selectedGenresCopy.find((g) => (g.name === evt.target.id))
     console.log(evt)
     if (found) {
       console.log(evt.target.id, "found", evt.target.checked)
@@ -62,23 +79,27 @@ const Books = (props) => {
       console.log(evt.target.id, "not found")
     }
 
-    setGenres(
-      genreCopy
+    setSelectedGenres(
+      selectedGenresCopy
     )
+
+    books.refetch({ genres: selectedGenresCopy.filter((g) => g.selected).map(g => g.name) })
   }
 
+  console.log(allGenres)
   const genreSelector =
     props.recs ? <></> :
-      genres.map((g) => {
+      allGenres.data.allGenres.map((g) => {
+        // console.log(g)
         return (
-          <label><input
+          <label key={g}><input
             type="checkbox"
-            name={g.name}
-            id={g.name}
-            key={g.name}
-            checked={g.selected}
+            name={g}
+            id={g}
+            key={g}
+            checked={selectedGenres.find((g2) => (g2.name === g)).selected}
             onChange={handleChange}
-          />{g.name}</label>
+          />{g}</label>
         )
       })
 
@@ -96,6 +117,7 @@ const Books = (props) => {
             <th>published</th>
           </tr>
           {
+            /*
             books.data.allBooks.filter((b) => {
               for (const bookGenre of b.genres) {
                 console.log("bookgenre", bookGenre)
@@ -104,7 +126,8 @@ const Books = (props) => {
                 if (foundBook && foundBook.selected) { console.log("foundGenre"); return true }
               }
               return false
-            }).map((a) => (
+            })*/
+            books.data.allBooks.map((a) => (
               <tr key={a.title}>
                 <td>{a.title}</td>
                 <td>{a.author.name} ({a.author.born})</td>
--- src/queries.js
+++ src/queries.js
@@ -10,8 +10,8 @@ export const ALL_AUTHORS = gql`
 `
 
 export const ALL_BOOKS = gql`
-  query {
-    allBooks {
+  query getBooks($genres: [String!]){
+    allBooks(genres: $genres) {
       title,
       author {
         name,
@@ -20,6 +20,12 @@ export const ALL_BOOKS = gql`
       published,
       genres
     }
+  }
+`
+
+export const ALL_GENRES = gql`
+  query {
+    allGenres
   }
 `
 
